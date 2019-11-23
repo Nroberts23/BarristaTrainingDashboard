c13f01fa339617421f349e0edbdf4be0a6c3c9d1
Nathan Roberts <nate.roberts23@gmail.com>
2019-11-22 21:04:53 -0800

    Added functionality to decrease weight on bar

--- main.js
+++ main.js
@@ -6,13 +6,45 @@ document.getElementById('startStop').addEventListener("click", function(){
 });
 
 var drinks_completed = {};
-var curDrink = {};
-curDrink['drip'] = [];
+var totalWeight = 0
+var drinkQueue = [];
+
+
 
 document.getElementById('drip').addEventListener('click', function(){
     var now = new Date().getTime();
-    curDrink['drip'].push(now);
-    document.getElementById('here').innerHTML = 'Latest click: ' + curDrink['drip'];
-    console.log(curDrink['drip']);
+    drinkQueue.push([1, now]);
+    updateWeight();
 });
 
+document.getElementById('short').addEventListener('click', function(){
+    var now = new Date().getTime();
+    drinkQueue.push([2, now]);   
+    updateWeight();
+});
+
+document.getElementById('long').addEventListener('click', function(){
+    var now = new Date().getTime();
+    drinkQueue.push([3, now]);   
+    updateWeight();
+});
+
+document.getElementById('out').addEventListener('click', function(){
+    var out = drinkQueue[0][0];
+    drinkQueue = drinkQueue.splice(1);
+    totalWeight = totalWeight - out;
+    updateWeightText();
+});
+
+function updateWeight(){
+    var tot = 0;
+    drinkQueue.forEach(function(obj){
+       tot = tot + obj[0]; 
+    });
+    totalWeight = tot;
+    updateWeightText();
+}
+
+function updateWeightText(){
+    document.getElementById('here').innerHTML = 'TotalWeight: ' + totalWeight;
+}
