6a61314772bff5100db417e7cd034953e05a7dff
Nathan Roberts <nate.roberts23@gmail.com>
2019-11-22 16:04:13 -0800

    Added functionality to record timestamps

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-document.getElementById('startStop').addEventListener('click', function(){
-    alert('it works');
+var onBar = '';
+
+document.getElementById('startStop').addEventListener("click", function(){ 
+    onBar= prompt("Who's on bar?");
+    document.getElementById('pageTitle').innerHTML = onBar + "'s Performance Assessment";    
+});
+
+var drinks_completed = {};
+var curDrink = {};
+
+document.getElementById('drip').addEventListener('click', function(){
+    var now = new Date().getTime();
+    document.getElementById('here').innerHTML = 'Latest click' + now
 });
